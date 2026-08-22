072bd89babc49c83d911d081c24c6cdca3bcef29
Rik-pal <rikp7941@gmail.com>
2026-08-22 19:26:45 +0530

    Time: 2 ms (71.55%) | Memory: 9.4 MB (76.57%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        
+        const pair<int, string> values[] = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
+            {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"},
+            {1, "I"}
+        };
+        
+        string result;
+        
+        for (const auto& [val, sym] : values) {
+            while (num >= val) {
+                result += sym;
+                num -= val;
+            }
+        }
+        
+        return result;
+    }
+};
